9697093dd273ca44e2feee61de53cf86a82062da
KariminK <103779342+KariminK@users.noreply.github.com>
2024-10-14 20:38:31 +0200

    added redirect

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,6 @@
 function sendToMPV() {
   const url = document.URL;
+  if (!url.includes("youtube.com/watch")) return;
   const query = url.substring(url.indexOf("?") + 3, url.length);
   fetch("http://localhost:3000/" + query)
     .then((res) => {
@@ -8,9 +9,27 @@ function sendToMPV() {
     .catch((err) => {
       console.log(`ERROR: ${err}`);
     });
+  history.back();
 }
 
 chrome.action.onClicked.addListener((tab) => {
+  if (!tab.url.includes("chrome://")) {
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: sendToMPV,
+    });
+  }
+});
+
+chrome.runtime.onInstalled.addListener(() =>
+  chrome.contextMenus.create({
+    title: "Send to mpv",
+    contexts: ["selection"],
+    id: "sendToMPV",
+  })
+);
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
   if (!tab.url.includes("chrome://")) {
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
